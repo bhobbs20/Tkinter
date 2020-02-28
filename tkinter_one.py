abac59b7884919d2e0b60cb445cce5453f8fbf93
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-28 12:08:27 -0500

    tkinter form

--- tkinter_one.py
+++ tkinter_one.py
@@ -9,8 +9,41 @@ class ParentWindow(Frame):
 
         self.master = master
         self.master.resizable(width=False, height=False)
-        self.master.geometry("600x600")
+        self.master.geometry("{}x{}".format(700,400))
         self.master.title("learning tkinter")
+        self.master.config(bg="lightgrey")
+
+        self.varFName = StringVar()
+        self.varLName = StringVar()
+
+        self.lblFName = Label(self.master, text='First Name:', font=("HelVetica", 16), fg='black', bg="lightgray" )
+        self.lblFName.grid(row=0, column=0, padx=(30,0), pady=(30,0))
+
+        self.lblLName = Label(self.master, text='Last Name:', font=("HelVetica", 16), fg='black', bg="lightgray")
+        self.lblLName.grid(row=1, column=0, padx=(30,0), pady=(30,0))
+
+        self.lblDisplay = Label(self.master, text='', font=("HelVetica", 16), fg='black', bg="lightgray")
+        self.lblDisplay.grid(row=3, column=1, padx=(30,0), pady=(30,0))
+
+        self.txtFName = Entry(self.master, text=self.varFName, font=("HelVetica", 16), fg='black', bg="lightblue" )
+        self.txtFName.grid(row=0, column=1, padx=(30,0), pady=(30,0))
+
+        self.txtLName = Entry(self.master, text=self.varLName, font=("HelVetica", 16), fg='black', bg="lightblue")
+        self.txtLName.grid(row=1, column=1, padx=(30,0), pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text="Submit", width=10, height=2, bg="grey", command=self.submit)
+        self.btnSubmit.grid(row=2, column=1, padx=(0,0), pady=(30,0), sticky=NE)
+
+        self.btnCancel = Button(self.master, text="Cancel", width=10, height=2, command=self.cancel)
+        self.btnCancel.grid(row=2, column=1, padx=(0, 90), pady=(30, 0), sticky=NE)
+
+    def submit(self):
+        fn = self.varFName.get()
+        ln = self.varLName.get()
+        self.lblDisplay.config(text="Hello {} {}!".format(fn, ln))
+
+    def cancel(self):
+        self.master.destroy()
 
 
 if __name__ == "__main__":
